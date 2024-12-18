7d511fc314b1e0448ba71c2e0335fc85ad48b4ac
Eric Owiredu Akumia Lartey <erickirikou123@gmail.com>
2024-12-18 14:46:42 +0000

    resolved

--- pharmageorge-backend/models/testimonial.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const testimonialSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  text: { type: String, required: true },
-  imageUrl: { type: String },
-});
-
-module.exports = mongoose.model('Testimonial', testimonialSchema);
